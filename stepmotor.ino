fc14228f10f4393d924eb368a82cd193362108d6
asthomas13 <94497920+asthomas13@users.noreply.github.com>
2024-04-28 22:41:14 -0500

    Update stepmotor.ino

--- stepmotor.ino
+++ stepmotor.ino
@@ -1,12 +1,11 @@
-//Includes the Arduino Stepper Library
 #include <Stepper.h> //Loads the stepper library
 
 // Defines the number of steps per rotation
-const int stepsPerRevolution = 512;
+const int stepsPerRev = 512;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-Stepper myStepper = Stepper(stepsPerRevolution, A0, A2, A1, A3);
+Stepper myStepper = Stepper(stepsPerRev, A0, A2, A1, A3);
 
 const int stepDelay = 10;
 
@@ -24,21 +23,10 @@ void setup() {
 }
 
 void loop() {
-  // Rotate CW slowly at 5 RPM
-//  myStepper.step(stepsPerRevolution);
-//  delay(1000);
-//  myStepper.step(0);
-//  myStepper.step(abs(stepsPerRevolution));
-//  delay(1000);
-//  // Rotate CCW quickly at 10 RPM
-//  myStepper.setSpeed(10);
-//  myStepper.step(-stepsPerRevolution);
-//  delay(10);
-
-  clockwise(stepsPerRevolution);
+  clockwise(stepsPerRev);
   delay(1000);
 
-  counterclockwise(stepsPerRevolution);
+  counterclockwise(stepsPerRev);
   delay(1000);
 
   myStepper.step(0);
